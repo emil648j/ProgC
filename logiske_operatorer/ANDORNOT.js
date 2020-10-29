b5cc91ad942bc30e2d8c339818ac86a66758e6b9
emil648j <emilrubynielsen@gmail.com>
2020-10-29 15:07:56 +0100

    &&

--- logiske_operatorer/ANDORNOT.js
+++ logiske_operatorer/ANDORNOT.js
@@ -1,31 +1,25 @@
 function setup() {
-    createCanvas(1920, 1080);
+    createCanvas(500, 500);
 }
 
 function draw() {
-    background(220);
-    let Posx = mouseX
-    let Posy = mouseY + 50
-    let headradius = 25
+    background(220)
+    let posx = mouseX
+    let posy = mouseY
+    let r = 10
 
-    circle(Posx, Posy, headradius * 2)
-    circle(Posx - 10, Posy, 2)
-    circle(Posx + 10, Posy, 2)
-    line(Posx - 12, Posy + 15, Posx + 12, Posy + 10)
-    strokeWeight(3)
-    fill(0, 150, 0)
-    stroke(150, 0, 0)
+    circle(posx, posy, r)
+    line(250, 0, 250, 500)
+    line(0, 250, 500, 250)
+    strokeWeight(10)
 
-    let necklenght = 15
-    line(Posx, Posy + headradius, Posx, Posy + headradius + necklenght);
-
-    let bodyheight = 100
-    let bodywidth = 30
-    ellipse(Posx, Posy + headradius + necklenght + bodyheight / 2, bodywidth, bodyheight)
-
-    line(Posx, Posy + headradius + necklenght, Posx - 40, Posy + 90)
-    line(Posx, Posy + headradius + necklenght, Posx + 40, Posy + 90)
-
-    line(Posx, Posy + headradius + necklenght + bodyheight, Posx - 30, Posy + headradius + necklenght + bodyheight + 70)
-    line(Posx, Posy + headradius + necklenght + bodyheight, Posx + 30, Posy + headradius + necklenght + bodyheight + 70)
+    if (posx > 250) {
+        circle(posx, posy, r * 2)
+    }
+    if (posy > 250) {
+        circle(posx, posy, r * 2)
+    }
+    if (posy > 250 && posx > 250) {
+        circle(posx, posy, r * 4)
+    }
 }
